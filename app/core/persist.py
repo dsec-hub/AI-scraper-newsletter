eeb9d07be4bc33e069b3c38589507b7b05c8583a
Zynzx <refactorypython@gmail.com>
2025-12-23 15:13:11 +1100

    added checks and fall backs for if logs and output directory are missing

--- app/core/persist.py
+++ app/core/persist.py
@@ -13,7 +13,9 @@ class ScraperOutput:
         # License: CC BY-SA 4.0   
         path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '../..', 'output'))
 
-
+        if os.path.exists(path) == False:
+            os.mkdir(path)
+            
         with jsonlines.open(f'{path}/article.jsonl', mode='a') as article_records:
             article_records.write(str(data))
         
--- logs/readme.txt
+++
@@ -1,3 +0,0 @@
-Git does not track empty directories, therefore this file must exist. 
-
-This directory exists for logs coming from the scraper.
